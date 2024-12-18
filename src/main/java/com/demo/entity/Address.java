123b083c4eb81e84a61660cf79b002659427cf10
pri <priyanka2.saha2@gmail.com>
2024-12-18 11:48:27 -0500

    Updating all Pending changes

--- src/main/java/com/demo/entity/Address.java
+++ src/main/java/com/demo/entity/Address.java
@@ -1,7 +1,5 @@
 package com.demo.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
